a59c13f527cbfd9104fd1bbbcf19d1a3a87b8bf2
ionutsavin <ionutsavin6@gmail.com>
2025-03-10 16:42:32 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -56,7 +56,6 @@ const jwt = require('jsonwebtoken');
 
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
-    console.log(req.body);
 
     if (!username || !password) {
         return res.status(400).json({ error: 'Username and password are required' });
